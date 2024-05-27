f449553150b3439b64cd72a4c074158ee8b2df7f
Goh Wei Wen <goweiwen@gmail.com>
2024-05-27 15:41:05 +0800

    feat: add logger

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,39 @@ mod miyoo;
 
 use anyhow::Result;
 use clap::Parser;
+use log::{info, LevelFilter};
+use simple_logger::SimpleLogger;
 
 slint::include_modules!();
 
 #[derive(Debug, Parser)]
 #[command(name = "vinyl", version, about, long_about = None)]
 #[command(bin_name = "vinyl")]
-struct VinylCli {}
+struct VinylCli {
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+}
 
 fn main() -> Result<()> {
-    let _ = VinylCli::parse();
+    let args = VinylCli::parse();
+
+    SimpleLogger::new()
+        .with_level(
+            #[cfg(debug_assertions)]
+            if args.verbose {
+                LevelFilter::Debug
+            } else {
+                LevelFilter::Info
+            },
+            #[cfg(not(debug_assertions))]
+            if args.verbose {
+                LevelFilter::Info
+            } else {
+                LevelFilter::Warn
+            },
+        )
+        .init()
+        .unwrap();
 
     run()?;
 
