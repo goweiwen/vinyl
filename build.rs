5f108250586de7412b677616b848e0985ea83f48
Goh Wei Wen <goweiwen@gmail.com>
2024-05-28 22:35:54 +0800

    feat: use fluent style

--- build.rs
+++ build.rs
@@ -3,11 +3,15 @@ fn main() {
     slint_build::compile_with_config(
         "ui/main.slint",
         slint_build::CompilerConfiguration::new()
+            .with_style("fluent".into())
             .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
     )
     .unwrap();
 
     #[cfg(feature = "simulator")]
-    slint_build::compile_with_config("ui/main.slint", slint_build::CompilerConfiguration::new())
-        .unwrap();
+    slint_build::compile_with_config(
+        "ui/main.slint",
+        slint_build::CompilerConfiguration::new().with_style("fluent".into()),
+    )
+    .unwrap();
 }
